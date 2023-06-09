debc14ad320d3d41a67b9eda7fe9e6bd92763f64
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-06-10 00:11:25 +0530

    Creating Sub-Collection message inside rooms and sending messageto the firestore

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
@@ -7,19 +7,29 @@ import "./css/chat.css";
 import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
 import MicIcon from "@material-ui/icons/Mic";
 import { useParams } from "react-router-dom";
-import { doc,getDoc } from "firebase/firestore";
+import { doc,getDoc,serverTimestamp,setDoc } from "firebase/firestore";
 import db from "./firebase";
 
 function Chat() {
+  const messageInputRef = useRef()
   const { roomId } = useParams();
   const [room,setroom] = useState("")
-  const docRef = doc(db, "rooms", roomId);
-
+  
   useEffect(()=>{
     if(roomId){
+    const docRef = doc(db, "rooms", roomId);
     getDoc(docRef).then((data)=>setroom(data.data().name))
     }
-  },[roomId,docRef])
+  },[roomId])
+
+
+  const submitHandler = (e) =>{
+    e.preventDefault()
+    const enteredMessage = messageInputRef.current.value
+    const docRef = doc(db, "rooms", roomId, "message","message");
+    setDoc(docRef,{message: enteredMessage, timestamp:serverTimestamp()})
+    messageInputRef.current.value = ""
+  }
 
 
   return (
@@ -63,9 +73,9 @@ function Chat() {
       <div className="chat__footer">
         <EmojiEmotionsIcon />
         <AttachFileIcon />
-        <form>
-          <input type="text" placeholder="Type your message" />
-          <input type="submit" />
+        <form onSubmit={submitHandler}>
+          <input type="text" placeholder="Type your message" ref={messageInputRef}/>
+          <input type="submit"/>
         </form>
         <MicIcon />
       </div>
